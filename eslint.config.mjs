8fe2aa47b7f5186d0be05a97d16233b24e4b0cfc
郑涛 <wavesbig@qq.com>
2025-11-12 11:42:09 +0800

    fix: 修改校验规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,3 @@
 import { config } from '@n8n/node-cli/eslint';
 
-
-export default [
-  // 引入 n8n 的默认配置
-  config,
-
-  // 添加一个新的配置对象来覆盖特定规则
-  {
-    rules: {
-      '@n8n/community-nodes/no-restricted-imports': 'off',
-    }
-  }
-];
+export default config
